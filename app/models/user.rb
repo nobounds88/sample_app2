faeb9804796c99c4e91958b39d3ec5941c733a4d
EC2 Default User <ec2-user@ip-172-31-0-59.ap-southeast-1.compute.internal>
2018-02-23 10:18:25 +0000

    Finish user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   has_many :followers, through: :reverse_relationships, source: :follower
   
   def feed
-    Micropost.where("user_id = ?", id)
+    Micropost.from_users_followed_by(self)
   end
   
   def following?(other_user)
